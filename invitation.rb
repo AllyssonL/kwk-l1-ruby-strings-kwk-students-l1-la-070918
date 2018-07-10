5658386b8f2129375725ca3e31f9ab0de794276a
AllyssonL <allyssonlingad@gmail.com>
2018-07-10 16:48:56 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -8,11 +8,11 @@ puts "what is your name?"
 guest = gets.strip
 
 puts "what is the name your event?"
-Event_name = gets.strip
+party = gets.strip
 
 
  puts "Dear #{guest},
-You are cordially invited to the #{Event_name} on October 31 at 6pm. Please RSVP no later than October 30.
+You are cordially invited to the #{party} on October 31 at 6pm. Please RSVP no later than October 30.
 
 Sincerely,
  Harry Potter"
