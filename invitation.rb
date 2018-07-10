b0b6fae908408f070c1597dcdc927c6c82261a66
AllyssonL <allyssonlingad@gmail.com>
2018-07-10 16:33:57 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,16 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+def letter
+Dear guest,
+
+You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+Sincerely,
+
+Harry Potter
+end
+
+letter 
+letter 
