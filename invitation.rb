784202cbf71eb82fb95610a31789227016752b44
AllyssonL <allyssonlingad@gmail.com>
2018-07-10 16:44:36 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,8 +3,11 @@
 # Try starting out with puts'ing a string.
 
 def letter
-  
- puts "Dear guest,
+puts "what is your name?"
+guest = gets.strip
+
+
+ puts "Dear #{guest},
 You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
 
 Sincerely,
