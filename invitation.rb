dcace50396575a42438bbe0db821a184e7f33b96
AllyssonL <allyssonlingad@gmail.com>
2018-07-10 16:34:58 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,6 +3,7 @@
 # Try starting out with puts'ing a string.
 
 def letter
+  
 Dear guest,
 
 You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
