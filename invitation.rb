dec7a663c069b4bb77d466f52ac6ea12c5645913
AllyssonL <allyssonlingad@gmail.com>
2018-07-10 16:48:16 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -7,9 +7,12 @@ def letter
 puts "what is your name?"
 guest = gets.strip
 
+puts "what is the name your event?"
+Event_name = gets.strip
+
 
  puts "Dear #{guest},
-You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+You are cordially invited to the #{Event_name} on October 31 at 6pm. Please RSVP no later than October 30.
 
 Sincerely,
  Harry Potter"
